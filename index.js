c356c215fe894800f45d7a429f7e8f74c340fd3f
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-17 21:04:23 +0000

    Add checkWinner and playRound functions

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log('hola'); 
+
+const options = ["rock", "paper", "scissors"]; 
+
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random() * options.length)]; 
+    return choice; 
+
+}
+
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie"; 
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") || 
+        (playerSelection == "paper"  && computerSelection == "rock")
+
+){
+    return "Player"; 
+}
+
+else {
+    return "Computer"; 
+}
+
+
+
+
+
+function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection); 
+    if(result == "Tie"){
+        return "It's a Tie!"
+    }
+    else if(result == "Player") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+    else{
+        return `You Lose! ${computerSelection} beast ${playerSelection}`
+    }
+}
+
+getComputerChoice(); 
+
